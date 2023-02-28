20e48ad902426d6c11d87b0624aaa58737d66bd1
Pascal Diehm <pdiehm8@gmx.de>
2023-02-28 19:11:39 +0100

    Small performance update - Removed fallback parameter to master expose function (not required) - Processing save file line-by-line (instead every character) - Continuing search for next token where the last search ended, enabling O(n) runtime

--- serializable.hpp
+++ serializable.hpp
@@ -11,6 +11,7 @@
 #include <regex>
 #include <sstream>
 #include <string>
+#include <type_traits>
 #include <vector>
 
 namespace serializable {
@@ -18,7 +19,7 @@ class Serializable {
   public:
     Serializable()                               = default;
     Serializable(const Serializable&)            = default;
-    Serializable(Serializable&&)                 = delete;
+    Serializable(const Serializable&&)           = delete;
     Serializable& operator=(const Serializable&) = default;
     Serializable& operator=(Serializable&&)      = delete;
     virtual ~Serializable()                      = default;
@@ -26,31 +27,33 @@ class Serializable {
     inline std::string serialize() {
         // Serialize data
         serialized.clear();
-        state = State::WRITING;
+        state         = State::WRITING;
+        lastSearchEnd = serialized.begin();
         exposed();
         state = State::IDLE;
 
         // Return serialized string
-        return serialized;
+        return getSerialized();
     }
 
     inline bool check(const std::string& data) {
-        // Ser serialized string
-        serialized = data;
+        // Set serialized data
+        setSerialized(data);
 
         // Perform check
         return check();
     }
 
     inline bool deserialize(const std::string& data) {
-        // Set serialized string
-        serialized = data;
+        // Set serialized data
+        setSerialized(data);
 
-        // Check data
+        // Run checks
         if(!check()) return false;
 
         // Deserialize data
-        state = State::READING;
+        state         = State::READING;
+        lastSearchEnd = serialized.begin();
         exposed();
         state = State::IDLE;
 
@@ -92,67 +95,67 @@ class Serializable {
     inline void expose(const char* name, bool& value) {
         expose<bool>(
             "BOOL", name, value, [](const bool& b) { return std::string(b ? "true" : "false"); },
-            [](const std::string& str) { return str == "true"; }, false);
+            [](const std::string& str) { return str == "true"; });
     }
 
     inline void expose(const char* name, char& value) {
         expose<char>(
             "CHAR", name, value, [](const char& c) { return std::to_string(c); },
-            [](const std::string& str) { return static_cast<char>(std::stoi(str)); }, 0);
+            [](const std::string& str) { return static_cast<char>(std::stoi(str)); });
     }
 
     inline void expose(const char* name, unsigned char& value) {
         expose<unsigned char>(
             "UCHAR", name, value, [](const unsigned char& c) { return std::to_string(c); },
-            [](const std::string& str) { return static_cast<unsigned char>(std::stoul(str)); }, 0);
+            [](const std::string& str) { return static_cast<unsigned char>(std::stoul(str)); });
     }
 
     inline void expose(const char* name, short& value) {
         expose<short>(
             "SHORT", name, value, [](const short& s) { return std::to_string(s); },
-            [](const std::string& str) { return static_cast<short>(std::stoi(str)); }, 0);
+            [](const std::string& str) { return static_cast<short>(std::stoi(str)); });
     }
 
     inline void expose(const char* name, unsigned short& value) {
         expose<unsigned short>(
             "USHORT", name, value, [](const unsigned short& s) { return std::to_string(s); },
-            [](const std::string& str) { return static_cast<unsigned short>(std::stoul(str)); }, 0);
+            [](const std::string& str) { return static_cast<unsigned short>(std::stoul(str)); });
     }
 
     inline void expose(const char* name, int& value) {
         expose<int>(
             "INT", name, value, [](const int& i) { return std::to_string(i); },
-            [](const std::string& str) { return std::stoi(str); }, 0);
+            [](const std::string& str) { return std::stoi(str); });
     }
 
     inline void expose(const char* name, unsigned int& value) {
         expose<unsigned int>(
             "UINT", name, value, [](const unsigned int& i) { return std::to_string(i); },
-            [](const std::string& str) { return static_cast<unsigned int>(std::stoul(str)); }, 0);
+            [](const std::string& str) { return static_cast<unsigned int>(std::stoul(str)); });
     }
 
     inline void expose(const char* name, long& value) {
         expose<long>(
             "LONG", name, value, [](const long& i) { return std::to_string(i); },
-            [](const std::string& str) { return std::stol(str); }, 0);
+            [](const std::string& str) { return std::stol(str); });
     }
 
     inline void expose(const char* name, unsigned long& value) {
         expose<unsigned long>(
             "ULONG", name, value, [](const unsigned long& i) { return std::to_string(i); },
-            [](const std::string& str) { return std::stoul(str); }, 0);
+            [](const std::string& str) { return std::stoul(str); });
     }
 
     inline void expose(const char* name, float& value) {
         expose<float>(
             "FLOAT", name, value, [](const float& f) { return std::to_string(f); },
-            [](const std::string& str) { return std::stof(str); }, 0);
+            [](const std::string& str) { return std::stof(str); });
     }
 
     inline void expose(const char* name, double& value) {
         expose<double>(
             "DOUBLE", name, value, [](const double& d) { return std::to_string(d); },
-            [](const std::string& str) { return std::stod(str); }, 0);
+            [](const std::string& str) { return std::stod(str); });
     }
 
     inline void expose(const char* name, std::string& value) {
@@ -171,49 +174,49 @@ class Serializable {
         };
 
         // Expose
-        expose<std::string>("STRING", name, value, serialize, deserialize, "");
+        expose<std::string>("STRING", name, value, serialize, deserialize);
     }
 
     inline void expose(const char* name, Serializable& value) {
-        // Pack serialized data as child of another
+        // Wrap serialized data in brackets
         const auto packObject = [](const std::string& data) {
-            if(data.empty()) return std::string("{}"); // Base case
+            if(data.empty()) return std::string("{}");
 
-            std::string str = "\n" + data.substr(0, data.size() - 1); // Add newline at the front, remove final newline
+            std::string str = "\n" + data;                                       // Add newline at the front
             str             = std::regex_replace(str, std::regex("\n"), "\n\t"); // Indent every line
             str             = "{" + str + "\n}";                                 // Wrap in brackets
 
             return str;
         };
 
-        // Unpack previously packed object into serialized data
+        // Unwrap serialized data
         const auto unpackObject = [](const std::string& data) {
-            if(data == "{}") return std::string(); // Base case
+            if(data == "{}") return std::string();
 
-            std::string str = data.substr(1, data.size() - 2); // Remove brackets (final newline is not read)
+            std::string str = data.substr(1, data.size() - 2);                   // Remove brackets
             str             = std::regex_replace(str, std::regex("\n\t"), "\n"); // Remove one indentation per line
-            str             = str.substr(1, str.size() - 1) + "\n"; // Remove first newline, add newline to the end
+            str             = str.substr(1, str.size() - 1);                     // Remove first newline
 
             return str;
         };
 
         // Get object prefix
-        const std::string prefix = std::string("OBJECT ").append(name).append(" = ");
+        std::string prefix;
+        prefix.append("OBJECT ").append(name).append(" = ");
 
         // Default checking behaviour
         if(state == State::CHECKING) {
-            if(auto match = find(prefix)) good &= typecheck(value, match.value().substr(prefix.size()));
+            if(auto match = find(prefix)) good &= typecheck(value, unpackObject(match.value().substr(prefix.size())));
             else good = false;
         }
 
-        // If writing: Write value
+        // If writing: Append value to serializable
         else if(state == State::WRITING)
-            serialized.append(prefix).append(packObject(value.serialize())).append("\n");
+            addSerialized(prefix.append(packObject(value.serialize())));
 
         // If reading: Read into value
         else if(state == State::READING)
-            if(const auto match = find(prefix, true))
-                value.deserialize(unpackObject(match.value().substr(prefix.size())));
+            if(const auto match = find(prefix)) value.deserialize(unpackObject(match.value().substr(prefix.size())));
     }
 
     template <typename T>
@@ -221,80 +224,44 @@ class Serializable {
     inline void expose(const char* name, T& value) {
         expose<T>(
             "ENUM", name, value, [](const T& t) { return std::to_string(static_cast<int>(t)); },
-            [](const std::string& str) { return static_cast<T>(std::stoi(str)); }, value);
+            [](const std::string& str) { return static_cast<T>(std::stoi(str)); });
     }
 
   private:
-    enum class State { IDLE, CHECKING, READING, WRITING } state{};
+    enum class State { IDLE, CHECKING, WRITING, READING } state{};
     bool good{};
-    std::string serialized;
+    std::vector<std::string> serialized;
+    std::vector<std::string>::iterator lastSearchEnd;
+
+    inline std::string getSerialized() {
+        std::string str;
+        for(const auto& line : serialized) str.append(line).append("\n");
+        if(!str.empty()) str.pop_back();
+        return str;
+    }
+
+    inline void addSerialized(const std::string& data) {
+        std::string line;
+        std::stringstream stream(data);
+        while(std::getline(stream, line)) serialized.push_back(line);
+        lastSearchEnd = serialized.begin();
+    }
+
+    inline void setSerialized(const std::string& data) {
+        serialized.clear();
+        addSerialized(data);
+    }
 
     inline bool check() {
         // Check data
-        good  = true;
-        state = State::CHECKING;
+        good          = true;
+        state         = State::CHECKING;
+        lastSearchEnd = serialized.begin();
         exposed();
         state = State::IDLE;
 
         // Return result
         return good;
-    }
-
-    template <typename T>
-    inline void expose(const char* type, const char* name, T& value, std::string (*serialize)(const T& t),
-                       T (*deserialize)(const std::string& str), const T& fallback) {
-        const std::string prefix = std::string(type).append(" ").append(name).append(" = ");
-
-        // If checking: See if value is available
-        if(state == State::CHECKING) {
-            if(auto match = find(prefix)) good &= typecheck(type, match.value().substr(prefix.size()), deserialize);
-            else good = false;
-        }
-
-        // If reading: Try to find the value, otherwise use fallback
-        else if(state == State::READING) {
-            if(const auto match = find(prefix, true)) value = deserialize(match.value().substr(prefix.size()));
-            else value = fallback;
-        }
-
-        // If writing: Append value to serialized data
-        else if(state == State::WRITING)
-            serialized.append(prefix).append(serialize(value)).append("\n");
-    }
-
-    inline std::optional<std::string> find(const std::string& prefix, bool remove = false) {
-        // Find start of object
-        const std::size_t begin = serialized.find(prefix);
-        if(begin == std::string::npos) return std::nullopt;
-
-        // Find end of object
-        bool inString = false;
-        int depth     = 0;
-        for(std::size_t pos = begin; pos < serialized.size(); pos++) {
-            const char c = serialized.at(pos);
-
-            // Finish if end is found
-            if(c == '\n' && depth == 0 && !inString) {
-                // If data should not be removed: Return value
-                if(!remove) return serialized.substr(begin, pos - begin);
-
-                // Erase data and return value
-                std::string result = serialized.substr(begin, pos - begin);
-                serialized.erase(begin, pos - begin + 1);
-                return result;
-            }
-
-            // Ignore everything inside string literals
-            if(c == '"') inString = !inString;
-            if(inString) continue;
-
-            // Increment depth
-            if(c == '{') depth++;
-            else if(c == '}') depth--;
-        }
-
-        // Failed to find end of object
-        return std::nullopt;
     }
 
     template <typename T>
@@ -327,6 +294,76 @@ class Serializable {
     }
 
     inline static bool typecheck(Serializable& object, const std::string& data) { return object.check(data); };
+
+    inline std::optional<std::string> find(const std::string& prefix) {
+        // Find line starting with prefix
+        const auto prefixPredicate = [&prefix](const std::string& s) { return s.starts_with(prefix); };
+        auto begin                 = std::find_if(lastSearchEnd, serialized.end(), prefixPredicate);
+
+        // If line was not found, try again from the top, otherwise fail
+        if(begin == serialized.end()) begin = std::find_if(serialized.begin(), serialized.end(), prefixPredicate);
+        if(begin == serialized.end()) return std::nullopt;
+
+        // Set last search end to (at least) here
+        lastSearchEnd = begin;
+
+        // If looking for object, find end
+        if(prefix.starts_with("OBJECT")) {
+            // Trivial object on one line
+            if(begin->ends_with("{}")) return *begin;
+
+            // Collect object
+            std::string obj;
+            for(auto it = begin; it != serialized.end(); it++) {
+                obj.append(*it).append("\n");
+                if(it->starts_with("}")) {
+                    lastSearchEnd = it;
+                    return obj;
+                }
+            }
+        }
+
+        // If looking for string, find end
+        if(prefix.starts_with("STRING")) {
+            // Trivial string on one line
+            if(begin->ends_with("\"")) return *begin;
+
+            // Collect string
+            std::string str;
+            for(auto it = begin; it != serialized.end(); it++) {
+                str.append(*it).append("\n");
+                if(it->ends_with("\"")) {
+                    lastSearchEnd = it;
+                    return str;
+                }
+            }
+        }
+
+        // Other types are defined on one line
+        return *begin;
+    }
+
+    template <typename T>
+    inline void expose(const char* type, const char* name, T& value, std::string (*serialize)(const T& t),
+                       T (*deserialize)(const std::string& str)) {
+        // Construct prefix
+        std::string prefix;
+        prefix.append(type).append(" ").append(name).append(" = ");
+
+        // If checking: See if value is available
+        if(state == State::CHECKING) {
+            if(auto match = find(prefix)) good &= typecheck(type, match.value().substr(prefix.size()), deserialize);
+            else good = false;
+        }
+
+        // If writing: Append value to serialized
+        else if(state == State::WRITING)
+            addSerialized(prefix.append(serialize(value)));
+
+        // If reading: Try to find the value
+        else if(state == State::READING)
+            if(const auto match = find(prefix)) value = deserialize(match.value().substr(prefix.size()));
+    }
 };
 
 template <typename T, std::size_t size> class Array : public Serializable, public std::array<T, size> {
